0e0b9b141837914efb97de4301954ffb889c458d
kayn0111034 <0111034@student.isf.edu.hk>
2021-10-17 02:14:10 +0800

    updated navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,18 @@
 const navbar = () => {
   return (
       <div>
-          <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-            <a class="navbar-brand" href="#"><b>TeamName</b></a>
+          <nav class="navbar navbar-expand-md d-flex navbar-dark bg-dark p-2">
+            <a class="navbar-brand" href="/index"><b>TeamName</b></a>
             <ul class="navbar-nav flex-grow-1 navbar-collapse collapse">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
-                      <a class="nav-link" href="#">Covid Quiz <span class="sr-only">(current)</span></a>
+                      <a class="nav-link" href="/CovidQuiz">Covid Quiz <span class="sr-only"></span></a>
                     </li>
                     <li class="nav-item active">
-                      <a class="nav-link" href="#">Self-Diagnosis</a>
+                      <a class="nav-link" href="/SelfDiagnosis">Self-Diagnosis</a>
                     </li>
                     <li class="nav-item active">
-                      <a class="nav-link" href="#">About Us</a>
+                      <a class="nav-link" href="/aboutus">About Us</a>
                     </li>
                 </ul>
               </ul> 
