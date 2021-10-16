4b7d5ef951ebfc4911e5d4894afebb4cbe0af050
Christine Zhao <christine.zhao@mail.utoronto.ca>
2021-10-16 13:22:37 -0400

    add links to navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,27 @@
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+
 const navbar = () => {
   return (
       <div>
           <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+            <Link to="/home">
             <a class="navbar-brand" href="#"><b>TeamName</b></a>
+            </Link>
             <ul class="navbar-nav flex-grow-1 navbar-collapse collapse">
                 <ul class="navbar-nav">
                     <li class="nav-item active">
+                      <Link to="/quiz">
                       <a class="nav-link" href="#">Covid Quiz <span class="sr-only">(current)</span></a>
+                      </Link>
                     </li>
                     <li class="nav-item active">
+                      <Link to="/selfdiagnosis">
                       <a class="nav-link" href="#">Self-Diagnosis</a>
+                      </Link>
                     </li>
                     <li class="nav-item active">
-                      <a class="nav-link" href="#">About Us</a>
+                      <Link to="/aboutus"><a class="nav-link" href="#">About Us</a></Link>
                     </li>
                 </ul>
               </ul> 
