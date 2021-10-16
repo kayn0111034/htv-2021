7c17ecdf2ff5ad8f75d15152763f87545b8c2b70
Christine Zhao <christine.zhao@mail.utoronto.ca>
2021-10-16 14:55:10 -0400

    quiz

--- src/components/CovidQuiz.jsx
+++ src/components/CovidQuiz.jsx
@@ -1,9 +1,17 @@
-const CovidQuiz = () => {
+import QuizQuestion from './QuizQuestion'
+
+const CovidQuiz = ({questions}) => {
     return (
         <div>
             <div>
                 <h1>Covid Quiz</h1>
             </div>
+            <div>
+                {questions.map((q) =>(
+                    <QuizQuestion question={q}></QuizQuestion>
+                ))}
+            </div>
+
         </div>
     )
 }
