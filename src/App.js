66fbb5fbc0e314326458432b4f3579d6e09ac8c2
kayn0111034 <0111034@student.isf.edu.hk>
2021-10-17 00:53:32 +0800

    added navbar.jsx, import bootstrap into App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
