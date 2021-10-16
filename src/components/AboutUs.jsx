f6df14800bc7f8dacec894a15455f1d388749529
kayn0111034 <0111034@student.isf.edu.hk>
2021-10-17 03:00:36 +0800

    Added stuff to the about us page

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,6 +1,39 @@
 const AboutUs = () => {
     return (
         <div>
+           <h1>About Us</h1>
+           <button type="button" class="btn btn-primary">
+               We are a team of 4 participating in Hack the Valley 5. <br/>
+           For this hackathon, we developed a project that aims to <br/>
+           help children gain a better understanding Covid-19. To do <br/> 
+           this, we gave the information about covid in a child-friendly<br/>
+           way, and also gave them a fun quiz to test their memory and <br/>
+           strengthen their knowledge of Covid and its effects, so they <br/>
+           can stay safe.
+           </button>
+           
+           
+
+           <div>
+               <h2>Alex 1?1</h2>
+               <div>short bio</div>
+            </div>
+           <div>
+               <h2>Christine Zhao</h2>
+               <div>short bio</div>
+               </div>
+           <div>
+               <h2>Wesley Chui</h2>
+                <div>
+                    Wesley is a junior in highschool.
+                    He likes to participate in hackathons and is proficient at Python, HTML and CSS.
+                </div>
+               </div>
+            <div>
+               <h2>William Zhang</h2>
+               <div>short bio</div>
+            </div>
+
         </div>
     )
 }
