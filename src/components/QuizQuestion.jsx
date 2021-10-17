5e9a1619c6cff823aa99ec119430459cf596ee05
Christine Zhao <christine.zhao@mail.utoronto.ca>
2021-10-16 21:10:15 -0400

    centered quiz

--- src/components/QuizQuestion.jsx
+++ src/components/QuizQuestion.jsx
@@ -5,27 +5,30 @@ const QuizQuestion = ( {question, onClick}) => {
     }
 
     return (
-        <div class=" p-1 m-2">
+        <div class=" p-1 m-2 quiz-question">
             <h1>{question.question}</h1>
-            
-            {question.completed ?
-                <div>
-                    {question.answers.map((ans)=>(
+            <div class="question-box">
+                {question.completed ?
+                    <div>
+                        {question.answers.map((ans)=>(
 
-                        <div className={`btn ${question.answers[question.correct - 1] == ans ? " correctAnswer" : ""}`}>{ans}</div>
-                    ))}
+                            <div className={`btn quiz-option ${question.answers[question.correct - 1] == ans ? " correctAnswer" : ""}`}>{ans}</div>
+                        ))}
 
-                    <div>Correct!</div>
-                </div>  
-                : <div>
-                    {question.answers.map((ans)=>(
-                            <button className="btn" onClick={()=>{onClick(question.id, ans)}}>{ans}</button>
-                    ))}
-                    {question.attempted ?
-                        <div>That's not quite it. Try it again!</div>
-                    :null
-                    }
-                    </div>}
+                        <div>Correct!</div>
+                    </div>  
+                    : <div>
+                        {question.answers.map((ans)=>(
+                                <button className="btn quiz-option " onClick={()=>{onClick(question.id, ans)}}>{ans}</button>
+                        ))}
+                        {question.attempted ?
+                            <div>That's not quite it. Try it again!</div>
+                        :null
+                        }
+                    </div>
+                }
+            </div>
+ 
             
 
             
