2799c6030491c4bab637ccc7fe743e6007a5d7bc
kayn0111034 <0111034@student.isf.edu.hk>
2021-10-17 13:01:42 +0800

    small changes

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -16,14 +16,17 @@ const AboutUs = () => {
            <div class="p-2 mx-3">
                 <h2 class="d-flex justify-content-start">Alex Teeter</h2>
                 <div>
-                Hello, I'm Alex Teeter! I'm currently going to UTSC and <br/> double majoring in Computer Science and Mathematics! <br/> I have interests in data science and data visualization!
+                Hello, I'm Alex Teeter! I'm currently going to UTSC and <br/> 
+                double majoring in Computer Science and Mathematics! <br/> 
+                I have interests in data science and data visualization!
                 </div>
             </div>
             
             <div>
                 <div class="p-2 mx-3">
                     <h2 class="d-flex justify-content-end">Christine Zhao</h2>
-                    <div class="d-flex justify-content-end">Hi, I'm Christine! I am a first year at UTSC in the Coop Computer Science program.</div>
+                    <div class="d-flex justify-content-end">
+                        Hi, I'm Christine! I am a first year at UTSC in the Coop Computer Science program.</div>
                     </div>
                 </div>
             
@@ -34,13 +37,13 @@ const AboutUs = () => {
                         Hi! I'm Wesley, and I'm a junior in high school. I like snowboarding <br/> 
                         and programming, and I know Python, HTML, CSS, while also learning a bit <br/>
                         of JavaScript. I plan on studying computer science for undergrad and <br/>
-                        am also interested in web development</div>
+                        am also interested in web development.</div>
                     </div>
                 </div>
 
                 <div class="p-2 mx-3">
                     <h2 class="d-flex justify-content-end">William Zhang</h2>
-                    <div class="d-flex justify-content-end">Hi, I'm William ! I am a first year at UTSC (i wish lmao)</div>
+                    <div class="d-flex justify-content-end">Hi, I'm William ! I am a first year at UTSC</div>
                 </div>
               
         </div>
