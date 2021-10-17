d80b9c8a4cd530b673ea7ec630db3eb59f1fd429
kayn0111034 <0111034@student.isf.edu.hk>
2021-10-17 12:53:25 +0800

    changed aboutus

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,37 +1,48 @@
 const AboutUs = () => {
     return (
-        <div class=" p-2">
-           <h1>About Us</h1>
-           <button type="button" class="btn btn-primary">
-               We are a team of 4 participating in Hack the Valley 5. <br/>
-           For this hackathon, we developed a project that aims to <br/>
-           help children gain a better understanding Covid-19. To do <br/> 
-           this, we gave the information about covid in a child-friendly<br/>
-           way, and also gave them a fun quiz to test their memory and <br/>
-           strengthen their knowledge of Covid and its effects, so they <br/>
-           can stay safe.
-           </button>
+        <div class="p-2">
            
-
-           <div>
-               <h2>Alex Teeter</h2>
-               <div>Hello, I'm Alex Teeter! I'm currently going to UTSC and double majoring in Computer Science and Mathematics! I have interests in data science and data visualization!</div>
-            </div>
-           <div>
-               <h2>Christine Zhao</h2>
-               <div>Hi, I'm Christine! I am a first year at UTSC</div>
-               </div>
-           <div>
-               <h2>Wesley Chui</h2>
+           <h1 class="d-flex justify-content-center" >About Us</h1>
+           <div class="d-flex justify-content-center">
+                    We are a team of 4 participating in Hack the Valley 5. <br/>
+                For this hackathon, we developed a project that aims to <br/>
+                help children gain a better understanding Covid-19. To do <br/> 
+                this, we gave the information about covid in a child-friendly<br/>
+                way, and also gave them a fun quiz to test their memory and <br/>
+                strengthen their knowledge of Covid and its effects, so they <br/>
+                can stay safe.
+           </div>
+           
+           <div class="p-2 mx-3">
+                <h2 class="d-flex justify-content-start">Alex Teeter</h2>
                 <div>
-                   short bio
+                Hello, I'm Alex Teeter! I'm currently going to UTSC and <br/> double majoring in Computer Science and Mathematics! <br/> I have interests in data science and data visualization!
                 </div>
-               </div>
-            <div>
-               <h2>William Zhang</h2>
-               <div>Hi, I'm currently a first year at UTSC</div>
             </div>
+            
+            <div>
+                <div class="p-2 mx-3">
+                    <h2 class="d-flex justify-content-end">Christine Zhao</h2>
+                    <div class="d-flex justify-content-end">Hi, I'm Christine! I am a first year at UTSC</div>
+                    </div>
+                </div>
+            
+                <div>
+                <div class="p-2 mx-3">
+                    <h2 class="d-flex justify-content-start">Wesley Chui</h2>
+                    <div class="d-flex justify-content-start">
+                        Hi! I'm Wesley, and I'm a junior in high school. I like snowboarding <br/> 
+                        and programming, and I know Python, HTML, CSS, while also learning a bit <br/>
+                        of JavaScript. I plan on studying computer science for undergrad and <br/>
+                        am also interested in web development</div>
+                    </div>
+                </div>
 
+                <div class="p-2 mx-3">
+                    <h2 class="d-flex justify-content-end">William Zhang</h2>
+                    <div class="d-flex justify-content-end">Hi, I'm William ! I am a first year at UTSC (i wish lmao)</div>
+                </div>
+              
         </div>
     )
 }
