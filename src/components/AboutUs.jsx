cf54584bbc0dc1f3c4838cbe72d54a2af0f91c58
kayn0111034 <0111034@student.isf.edu.hk>
2021-10-17 13:34:03 +0800

    updated aboutus

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -26,7 +26,8 @@ const AboutUs = () => {
                 <div class="p-2 mx-3">
                     <h2 class="d-flex justify-content-end">Christine Zhao</h2>
                     <div class="d-flex justify-content-end">
-                        Hi, I'm Christine! I am a first year at UTSC in the Coop Computer Science program.</div>
+                        Hi, I'm Christine! I am a first year at UTSC in the Coop<br/>
+                        Computer Science program.</div>
                     </div>
                 </div>
             
@@ -34,19 +35,21 @@ const AboutUs = () => {
                 <div class="p-2 mx-3">
                     <h2 class="d-flex justify-content-start">Wesley Chui</h2>
                     <div class="d-flex justify-content-start">
-                        Hi! I'm Wesley, and I'm a junior in high school. I like snowboarding <br/> 
-                        and programming, and I know Python, HTML, CSS, while also learning a bit <br/>
-                        of JavaScript. I plan on studying computer science for undergrad and <br/>
-                        am also interested in web development.</div>
+                        Hi! I'm Wesley, and I'm a junior in high school. I like <br/>
+                        snowboarding and programming, and I know Python, <br/>
+                        HTML, CSS, while also learning a bit of JavaScript. <br/>
+                        I plan on studying computer science in university <br/>
+                        and am also interested in web development.</div>
                     </div>
                 </div>
 
                 <div class="p-2 mx-3">
                     <h2 class="d-flex justify-content-end">William Zhang</h2>
                     <div class="d-flex justify-content-end">
-                        Hi, I'm William and I'm currently attending UTSC studying Computer Science. 
-                        I have a basic understanding of Python, HTML, CSS, and Java, however I am
-                        still searching for my main interests.</div>
+                        Hi, I'm William and I'm currently attending UTSC <br/>
+                        studying Computer Science. I have a basic understanding <br/> 
+                        of Python, HTML, CSS, and Java, however I am still<br/>
+                         searching for my main interests.</div>
                 </div>
               
         </div>
